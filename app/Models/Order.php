d2afd33e0ebc9343e9b519571ebfb68a7e421674
dujianjun <v@jianwi.cn>
2020-07-06 18:32:33 +0800

    邮件调试

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -11,6 +11,7 @@ class Order extends Model
     public function generateCertificate()
     {
         $date = date('Y 年 m 月 d 日',$this->created_at->getTimestamp());
+
         return \Intervention\Image\Facades\Image::make("img/book.png")
             ->text("尊敬的 {$this->name} 同学, 感谢您于 {$date}",250,630,function($font) {
                 $font->file('font/msyhbd.ttf');
